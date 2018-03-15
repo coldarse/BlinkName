c5189061c9b1811d4ad10551b419dc987fefad2e
coldarse <37390180+coldarse@users.noreply.github.com>
2018-03-15 13:26:28 +1100

    Add files via upload

---
+++ blinkName.ino
@@ -0,0 +1,84 @@
+int LED = D7;
+
+void setup() {
+    pinMode(LED, OUTPUT);
+}
+
+void loop() {
+    //Letter 'J'
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
+    delay(3000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
+    delay(3000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
+    delay(3000);
+    digitalWrite(LED, LOW);
+    delay(2000);
+    //Letter 'O'
+    digitalWrite(LED, HIGH);
+    delay(3000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
+    delay(3000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
+    delay(3000);
+    digitalWrite(LED, LOW);
+    delay(2000);
+    //Letter 'R'
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
+    delay(3000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(2000);
+    //Letter 'D'
+    digitalWrite(LED, HIGH);
+    delay(3000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(2000);
+    //Letter 'A'
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
+    delay(3000);
+    digitalWrite(LED, LOW);
+    delay(2000);
+    //Letter 'N'
+    digitalWrite(LED, HIGH);
+    delay(3000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(2000);
+}
+
+
